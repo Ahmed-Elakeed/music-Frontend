24fd2e67dd501611f69a2cb624d35ba30477474d
Ahmed-Elakeed <57276446+Ahmed-Elakeed@users.noreply.github.com>
2022-02-21 19:22:25 +0200

    cc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GenreComponent } from './component/genre/genre.component';
+import { ArtistComponent } from './component/artist/artist.component';
+import { AlbumComponent } from './component/album/album.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import { AppRoutingModule } from './app-routing.module';
+import { ArtistFormComponent } from './component/artist/artist-form/artist-form.component';
+import { GenreFormComponent } from './component/genre/genre-form/genre-form.component';
+import { AlbumFormComponent } from './component/album/album-form/album-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GenreComponent,
+    ArtistComponent,
+    AlbumComponent,
+    NavbarComponent,
+    ArtistFormComponent,
+    GenreFormComponent,
+    AlbumFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
